95a18daa799999b48ee593a9c43b5e7bd0203a9d
Indira <indirasingh30092002@_gmail@.com>
2026-05-29 20:11:38 +0530

    Fix production backend API URL

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "/choreo-apis/chooseyouradventure/backend/v1/api"
+export const API_BASE_URL =
+"https://0e0592c2-9b88-49d5-bc1a-18507aa36c8f-dev.e1-us-east-azure.choreoapis.dev/chooseyouradventure/backend/v1.0/api";
